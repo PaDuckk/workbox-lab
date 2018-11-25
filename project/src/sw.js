71a25cec57def1a421c4605ea55cf021d6c1f6b3
PaDuckk <kang10019@naver.com>
2018-11-25 15:37:46 +0900

    optional: Add a customized cacheFirst cache strategy

--- project/src/sw.js
+++ project/src/sw.js
@@ -54,6 +54,27 @@ if (workbox) {
       });
   });
 
+  const cacheHandler = workbox.strategies.cacheFirst({
+    cacheName: 'posts-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 50,
+      })
+    ]
+  });
+
+  workbox.routing.registerRoute(/(.*)post(.*)\.html/, args => {
+    return cacheHandler.handle(args).then(response => {
+        if (response.status === 404) {
+          return caches.match('pages/404.html');
+        }
+        return response;
+      })
+      .catch(function(){
+        return caches.match('pages/offline.html');
+      });
+  });
+
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
